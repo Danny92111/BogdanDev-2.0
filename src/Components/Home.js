63a700ec2462e8e323c37f7ae3c2c752336d3a40
Danny92111 <danilescubogdan92@gmail.com>
2021-10-13 15:06:03 +0100

    Added meta info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,10 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
+import MetaImg from "../Media/img-OG-metaTags.jpg";
 
 function Home() {
+	console.log(MetaImg);
 	return (
 		<div id="Home" className="Home__container">
 			<div className="Home__miniContainer">
